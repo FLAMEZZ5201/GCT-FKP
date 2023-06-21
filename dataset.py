a04ffd724c97f0959c4d4e0121ce99d593cd3a50
FLAMEZZ <50903680+FLAMEZZ5201@users.noreply.github.com>
2023-06-21 19:27:08 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 data_path = "train_gsdata_rad.csv"
 
-# 完成数据集类
+# Complete the dataset class
 class MyDataset(Dataset):
     def __init__(self):
-        self.data = pd.read_csv(data_path).values  # DataFrame类型，通过values转换成numpy类型
+        self.data = pd.read_csv(data_path).values  # DataFrame type, values to numpy
 
     def __getitem__(self, index):
         """
-        必须实现，作用是:获取索引对应位置的一条数据
+        Gets a piece of data at the corresponding location of the index
         :param index:
         :return:
         """
@@ -19,7 +19,7 @@ class MyDataset(Dataset):
 
     def __len__(self):
         """
-        必须实现，作用是得到数据集的大小
+        Get dataset size 
         :return:
         """
         return len(self.data)
@@ -27,13 +27,13 @@ class MyDataset(Dataset):
     @staticmethod
     def to_tensor(data):
         """
-        将ndarray转换成tensor
+        ndarray to tensor
         :param data:
         :return:
         """
         return torch.from_numpy(data)
 
 if __name__ == "__main__":
-    data = MyDataset() # 实例化对象
-    print(data[1]) # 取第1条数据
-    print(len(data)) # 获取长度
+    data = MyDataset() 
+    print(data[1]) 
+    print(len(data))
